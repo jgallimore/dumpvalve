8b56b1a905ef6fa46ee83c451031592217e3b750
Jonathan Gallimore <jon@jrg.me.uk>
2012-08-28 22:43:45 +0100

    Changing how data is intercepted

--- src/main/java/uk/me/jrg/dumpvalve/DumpValve.java
+++ src/main/java/uk/me/jrg/dumpvalve/DumpValve.java
@@ -1,40 +1,211 @@
 package uk.me.jrg.dumpvalve;
 
+import org.apache.catalina.Session;
+import org.apache.catalina.Valve;
+import org.apache.catalina.authenticator.Constants;
+import org.apache.catalina.authenticator.SavedRequest;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.RequestFilterValve;
 import org.apache.coyote.ActionCode;
 import org.apache.coyote.InputBuffer;
 import org.apache.tomcat.util.buf.ByteChunk;
+import org.apache.tomcat.util.buf.MessageBytes;
+import org.apache.tomcat.util.http.MimeHeaders;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Locale;
 
 public class DumpValve extends RequestFilterValve {
     @Override
     public void invoke(Request request, Response response) throws IOException, ServletException {
 
-
-        final org.apache.coyote.Request coyoteRequest = request.getCoyoteRequest();
-        final InputBuffer inputBuffer = coyoteRequest.getInputBuffer();
-
-        ByteChunk body = new ByteChunk();
-        inputBuffer.doRead(body, coyoteRequest);
-
-        final byte[] bytes = body.getBytes();
         String postBody = "";
+        final org.apache.coyote.Request coyoteRequest = request.getCoyoteRequest();
 
-        if (bytes != null) {
-            postBody = new String(bytes, request.getCharacterEncoding());
+        if ("POST".equals(request.getMethod())) {
+            final InputBuffer inputBuffer = coyoteRequest.getInputBuffer();
+
+            ByteChunk body = new ByteChunk();
+            inputBuffer.doRead(body, coyoteRequest);
+
+            final byte[] bytes = body.getBytes();
+
+
+            if (bytes != null) {
+                postBody = new String(bytes, request.getCharacterEncoding());
+            }
+
+            request.getCoyoteRequest().getParameters().recycle();
+            request.getCoyoteRequest().getParameters().setQueryStringEncoding(
+                    request.getConnector().getURIEncoding());
+            coyoteRequest.action(ActionCode.ACTION_REQ_SET_BODY_REPLAY, body);
         }
+
+        final Valve valve = getNext();
+        valve.invoke(request, response);
 
         System.out.println("=============================================");
         System.out.println("URL: " + request.getRequestURI());
         System.out.println(postBody);
         System.out.println("=============================================");
+    }
 
+    /**
+     * Restore the original request from information stored in our session.
+     * If the original request is no longer present (because the session
+     * timed out), return <code>false</code>; otherwise, return
+     * <code>true</code>.
+     *
+     * @param request The request to be restored
+     * @param session The session containing the saved information
+     */
+    protected boolean restoreRequest(Request request, Session session)
+            throws IOException {
 
-        coyoteRequest.action(ActionCode.ACTION_REQ_SET_BODY_REPLAY, body);
-        getNext().invoke(request, response);
+        // Retrieve and remove the SavedRequest object from our session
+        SavedRequest saved = (SavedRequest)
+                session.getNote(Constants.FORM_REQUEST_NOTE);
+        session.removeNote(Constants.FORM_REQUEST_NOTE);
+        session.removeNote(Constants.FORM_PRINCIPAL_NOTE);
+        if (saved == null) {
+            return (false);
+        }
+
+        // Modify our current request to reflect the original one
+        request.clearCookies();
+        Iterator<Cookie> cookies = saved.getCookies();
+        while (cookies.hasNext()) {
+            request.addCookie(cookies.next());
+        }
+
+        String method = saved.getMethod();
+        MimeHeaders rmh = request.getCoyoteRequest().getMimeHeaders();
+        rmh.recycle();
+        boolean cachable = "GET".equalsIgnoreCase(method) ||
+                "HEAD".equalsIgnoreCase(method);
+        Iterator<String> names = saved.getHeaderNames();
+        while (names.hasNext()) {
+            String name = names.next();
+            // The browser isn't expecting this conditional response now.
+            // Assuming that it can quietly recover from an unexpected 412.
+            // BZ 43687
+            if (!("If-Modified-Since".equalsIgnoreCase(name) ||
+                    (cachable && "If-None-Match".equalsIgnoreCase(name)))) {
+                Iterator<String> values = saved.getHeaderValues(name);
+                while (values.hasNext()) {
+                    rmh.addValue(name).setString(values.next());
+                }
+            }
+        }
+
+        request.clearLocales();
+        Iterator<Locale> locales = saved.getLocales();
+        while (locales.hasNext()) {
+            request.addLocale(locales.next());
+        }
+
+        request.getCoyoteRequest().getParameters().recycle();
+        request.getCoyoteRequest().getParameters().setQueryStringEncoding(
+                request.getConnector().getURIEncoding());
+
+        // Swallow any request body since we will be replacing it
+        byte[] buffer = new byte[4096];
+        InputStream is = request.createInputStream();
+        while (is.read(buffer) >= 0) {
+            // Ignore request body
+        }
+
+        ByteChunk body = saved.getBody();
+
+        if (body != null) {
+            request.getCoyoteRequest().action
+                    (ActionCode.ACTION_REQ_SET_BODY_REPLAY, body);
+
+            // Set content type
+            MessageBytes contentType = MessageBytes.newInstance();
+
+            // If no content type specified, use default for POST
+            String savedContentType = saved.getContentType();
+            if (savedContentType == null && "POST".equalsIgnoreCase(method)) {
+                savedContentType = "application/x-www-form-urlencoded";
+            }
+
+            contentType.setString(savedContentType);
+            request.getCoyoteRequest().setContentType(contentType);
+        }
+
+        request.getCoyoteRequest().method().setString(method);
+
+        request.getCoyoteRequest().queryString().setString
+                (saved.getQueryString());
+
+        request.getCoyoteRequest().requestURI().setString
+                (saved.getRequestURI());
+        return (true);
+
+    }
+
+    /**
+     * Save the original request information into our session.
+     *
+     * @param request The request to be saved
+     * @param session The session to contain the saved information
+     * @throws IOException
+     */
+    protected void saveRequest(Request request, Session session)
+            throws IOException {
+
+        // Create and populate a SavedRequest object for this request
+        SavedRequest saved = new SavedRequest();
+        Cookie cookies[] = request.getCookies();
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                saved.addCookie(cookies[i]);
+            }
+        }
+        Enumeration<String> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            Enumeration<String> values = request.getHeaders(name);
+            while (values.hasMoreElements()) {
+                String value = values.nextElement();
+                saved.addHeader(name, value);
+            }
+        }
+        Enumeration<Locale> locales = request.getLocales();
+        while (locales.hasMoreElements()) {
+            Locale locale = locales.nextElement();
+            saved.addLocale(locale);
+        }
+
+        // May need to acknowledge a 100-continue expectation
+        request.getResponse().sendAcknowledgement();
+
+        ByteChunk body = new ByteChunk();
+        body.setLimit(request.getConnector().getMaxSavePostSize());
+
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        InputStream is = request.getInputStream();
+
+        while ((bytesRead = is.read(buffer)) >= 0) {
+            body.append(buffer, 0, bytesRead);
+        }
+
+        // Only save the request body if there is something to save
+        if (body.getLength() > 0) {
+            saved.setContentType(request.getContentType());
+            saved.setBody(body);
+        }
+
+        saved.setMethod(request.getMethod());
+        saved.setQueryString(request.getQueryString());
+        saved.setRequestURI(request.getRequestURI());
     }
 }
